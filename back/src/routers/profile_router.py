c550f0d9d3c3aa26a0cbb68bfcc6d799eb8d9500
hsmtDP-T <ittuyo.hashi244@gmail.com>
2025-09-19 02:39:04 +0900

    プロフィール取得および編集機能の実装を修正し、コードのコメントアウトを解除

--- back/src/routers/profile_router.py
+++ back/src/routers/profile_router.py
@@ -1,28 +1,34 @@
-# from fastapi import APIRouter, HTTPException, Request
-# from pydantic import BaseModel
-# from ..supabase.auth import login, sign
-# router = APIRouter(prefix="/profile", tags=["profile"])
+from fastapi import APIRouter, HTTPException, Request
+from pydantic import BaseModel
+router = APIRouter(prefix="/profile", tags=["profile"])
 
+class profileEditReq(BaseModel):
+    user_name: str
+    avatar_url: str
+    gender: str
+    age: int
+    comment: str
+    hobby: str 
 
-# @router.get("/me")
-# def profile(request: Request):
-#     token = request.session.get("access_token")
-#     if not token:
-#         raise HTTPException(status_code=400, detail="ログインtokenがありません")
-#     id = request.session.get("user_id")
-#     res = myProfileGet(id)
-#     if not res:
-#         raise HTTPException(status_code=400, detail="プロフィール取得失敗")
-#     return res
+@router.get("/me")
+def profile(request: Request):
+    token = request.session.get("access_token")
+    if not token:
+        raise HTTPException(status_code=400, detail="ログインtokenがありません")
+    id = request.session.get("user_id")
+    res = myProfileGet(id)
+    if not res:
+        raise HTTPException(status_code=400, detail="プロフィール取得失敗")
+    return res
 
-# @router.patch("/me")
-# def profileEdit(request: Request ):
+@router.patch("/me")
+def profileEdit(request: Request, body: profileEditReq ):
     
-#     token = request.session.get("access_token")
-#     if not token:
-#         raise HTTPException(status_code=400, detail="ログインtokenがありません")
-#     id = request.session.get("user_id")
-#     res = myProfileEdit(id, req: user_name, req: avatar_url)
-#     if not res:
-#         raise HTTPException(status_code=400, detail="プロフィール変更失敗")
-#     return res
+    token = request.session.get("access_token")
+    if not token:
+        raise HTTPException(status_code=400, detail="ログインtokenがありません")
+    id = request.session.get("user_id")
+    res = myProfileEdit( id, body.user_name,  body.avatar_url, body.gender, body.age, body.comment, body.hobby )
+    if not res:
+        raise HTTPException(status_code=400, detail="プロフィール変更失敗")
+    return res
