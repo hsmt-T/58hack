c642bf6ecd8e68ba2f251b81f5c1c5d45f31f58a
稲田陽向 <hinata@inadaakiramukinoMacBook-Air.local>
2025-09-21 04:27:10 +0900

    接続

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
-export const Login = () => { 
+export const Login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
